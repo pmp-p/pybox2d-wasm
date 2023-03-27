0408428058c2f9c156992471b4d852fd04a5167a
pmp-p <mail.peny@free.fr>
2023-03-27 19:26:05 +0200

    fix sample

--- library/main.py
+++ library/main.py
@@ -1,14 +1,7 @@
 import sys
 import asyncio
 import pygame
-try:
-    import i18n
-    import pygame_gui
-except:
-    shell = None
-
 import os
-import Box2D
 
 if sys.argv[-1].find("Box2D")>=0:
     arg = sys.argv[-1]
