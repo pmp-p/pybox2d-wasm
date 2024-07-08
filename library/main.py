75269d0a330ebc8f74f90245420fafa1be71bca8
Paul m. p. Peny <mail.peny@free.fr>
2024-07-08 15:33:01 +0200

    Update main.py

--- library/main.py
+++ library/main.py
@@ -3,6 +3,13 @@ import asyncio
 import pygame
 import os
 
+# /// script
+# dependencies = [
+#  "pygame-ce",
+#  "Box2D",
+# ]
+# ///
+
 if sys.argv[-1].find("Box2D")>=0:
     arg = sys.argv[-1]
 else:
